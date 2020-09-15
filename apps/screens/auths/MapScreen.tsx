b12e63c6b84dc184358cb0b97631785438873c1b
Tars <tars@gmail.com>
2020-09-15 20:52:18 +0800

    add image Picker

--- apps/screens/auths/MapScreen.tsx
+++ apps/screens/auths/MapScreen.tsx
@@ -1,26 +1,57 @@
-import React from "react";
-import { StyleSheet, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import { StyleSheet } from "react-native";
 import MapView, { Marker } from 'react-native-maps';
 import Avatar from "../../views/Avatar";
+import * as Permissions from 'expo-permissions';
+import * as Location from 'expo-location';
 
 const MapScreen = () => {
+
+
+  const [latitude, setLatitude] = useState<number>() // 緯度
+  const [longitude, setＬongitude] = useState<number>() // 經度
+
+  const getLocation = async () => {
+    let { status } = await Permissions.askAsync(Permissions.LOCATION);
+    if (status !== 'granted') {
+      alert('Permission to access location was denied')
+      // setErrorMsg('Permission to access location was denied');
+    } else {
+      const { coords } = await Location.getCurrentPositionAsync({});
+      const { latitude, longitude } = coords
+      setLatitude(latitude);
+      setＬongitude(longitude)
+    }
+  }
+
+  useEffect(() => {
+    getLocation()
+  }, []);
+
+  console.log("-------latitude-------", latitude)
+  console.log("-------longitude-------", longitude)
+
   return (
     <MapView
+      // provider={'google'}
       style={{ flex: 1 }}
       initialRegion={{
-        latitude: 37.78825,
-        longitude: -122.4324,
+        latitude: latitude ?? 37.78825,
+        longitude: longitude ?? -122.4324,
         latitudeDelta: 0.0922,
         longitudeDelta: 0.0421,
       }}
     >
-      <Marker coordinate={{
-        latitude: 37.78825,
-        longitude: -122.4324,
-      }}>
-        <Avatar uri={'https://i.ytimg.com/vi/uYwefisyGkY/maxresdefault.jpg'} size={60} />
-      </Marker>
-    </MapView>
+      {
+        latitude && longitude &&
+        < Marker coordinate={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+        }}>
+          <Avatar uri={'https://i.ytimg.com/vi/uYwefisyGkY/maxresdefault.jpg'} size={60} />
+        </Marker>
+      }
+    </MapView >
   );
 };
 
--- apps/screens/auths/UserProfileScreen.tsx
+++ apps/screens/auths/UserProfileScreen.tsx
@@ -1,6 +1,9 @@
 import React from "react";
-import { StyleSheet, View, Button } from "react-native";
+import { Button, StyleSheet, View } from "react-native";
 import firebase from "../../configs/Firebase";
+import Avatar from "../../views/Avatar";
+import * as ImagePicker from 'expo-image-picker';
+import * as Permissions from 'expo-permissions';
 
 const handleSignOut = () => {
   firebase
@@ -10,9 +13,37 @@ const handleSignOut = () => {
     .catch(error => console.log(error));
 };
 
+
+const pickImage = async () => {
+  try {
+    const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+    if (status !== 'granted') {
+      alert('Sorry, we need camera roll permissions to make this work!');
+    } else {
+      let result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.All,
+        allowsEditing: true,
+        aspect: [4, 3],
+        quality: 1,
+      });
+      if (!result.cancelled) {
+        console.log("-----result.uri-----", result?.uri)
+        // this.setState({ image: result.uri });
+      }
+      console.log(result);
+
+    }
+
+
+  } catch (E) {
+    console.log(E);
+  }
+};
+
 const UserProfileScreen = () => {
   return (
     <View style={styles.container}>
+      <Avatar uri={'https://i.ytimg.com/vi/uYwefisyGkY/maxresdefault.jpg'} size={60} onPress={pickImage} />
       <Button title="登出" onPress={handleSignOut} />
     </View>
   );
@@ -23,7 +54,7 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#fff",
     alignItems: "center",
-    justifyContent: "center"
+    paddingVertical: 20
   }
 });
 
